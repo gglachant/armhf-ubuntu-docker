2f02c910a58d8371076d7dd6100bb6b11ed1d617
Gabriel Glachant <gglachant@gmail.com>
2016-01-08 18:18:39 +0100

    Remove shell script error.

--- build.sh
+++ build.sh
@@ -61,7 +61,7 @@ cat /tmp/${ARCHIVE_NAME} | sudo docker import - $IMAGE_NAME
 rm /tmp/${ARCHIVE_NAME} /tmp/$aptConfPath -fR
 
 # Use qemu unless running on armv7l architecture
-if [ ! $ON_ARM -a ! -f /proc/sys/fs/binfmt_misc/arm ]; then
+if [ ! $ON_ARM=1 -a ! -f /proc/sys/fs/binfmt_misc/arm ]; then
   sudo sh -c 'echo ":arm:M::\x7fELF\x01\x01\x01\x00\x00\x00\x00\x00\x00\x00\x00\x00\x02\x00\x28\x00:\xff\xff\xff\xff\xff\xff\xff\x00\xff\xff\xff\xff\xff\xff\xff\xff\xfe\xff\xff\xff:/usr/bin/qemu-arm-static:" >/proc/sys/fs/binfmt_misc/register'
 fi
 
